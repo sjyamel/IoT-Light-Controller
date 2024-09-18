0534e15c3e368bd9f688861970e23df1d9c04bc1
Mr-Teee <ahmadabdulrazaq05011@gmail.com>
2024-09-18 11:00:42 +0000

    updates

--- IoT Light Controller interface/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import vercel from 'vite-plugin-vercel';
- 
-export default defineConfig({
-  server: {
-    port: process.env.PORT,
-  },
-  plugins: [vercel()],
-});
