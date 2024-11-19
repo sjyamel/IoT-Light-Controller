b55dd0ba788522767149920f91aa081191068d6b
Sulaiman Jabir <128595145+sjyamel@users.noreply.github.com>
2024-11-19 09:22:20 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -31,30 +31,32 @@ const fetchLedStatus = async ()=>{
 const toggleOn = async () => {
   try {
     // Send the request to update the LED status
-    const response = await fetch("https://iot-api-delta.vercel.app/update-led-status", {
-      method: "POST", // assuming it's a POST request, change it if it's GET or other
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ status: isOn ? "off" : "on" }) // Send the correct status in the request
-    });
+    // const response = await fetch("https://iot-api-delta.vercel.app/update-led-status", {
+    //   method: "POST", // assuming it's a POST request, change it if it's GET or other
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({ status: isOn ? "off" : "on" }) // Send the correct status in the request
+    // });
     const status = isOn ? "on": "off";
     await fetch(`http://192.168.1.220/led/${status}`)
   .then(res => res.text())
-  .then(data=>{
+  .then(data => {
     console.log(data);
-  }).catch(error => {
+  })
+  .catch(error => {
     console.error("Error", error);
   });
 
+
     // Check if the request was successful
-    if (response.ok) {
-      // If the request was successful, toggle the LED status
-      console.log(await response.json());
-      setIsOn(!isOn);
-    } else {
-      console.error("Failed to update LED status");
-    }
+    // if (response.ok) {
+    //   // If the request was successful, toggle the LED status
+    //   console.log(await response.json());
+    //   setIsOn(!isOn);
+    // } else {
+    //   console.error("Failed to update LED status");
+    // }
   } catch (error) {
     console.error("Error updating LED status:", error);
   }
